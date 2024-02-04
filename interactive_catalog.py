a8a3c2d829b709d05e585b841baa0018c288b9ae
Ka Lok (Rico) Lo <rico.kaloklo@gmail.com>
2024-02-05 00:38:14 +0100

    Use multiselect instead for lens type

--- interactive_catalog.py
+++ interactive_catalog.py
@@ -10,10 +10,12 @@ st.title("Interactive Web App for lenscat")
 # This catalog
 catalog = lenscat.catalog
 
+catalog_img = st.empty() # Placeholder
+
 # Search by lens type
-lens_type_option = st.selectbox(
+lens_type_option = st.multiselect(
     "Lens type",
-    (None, *lenscat.Catalog._allowed_type)
+    lenscat.Catalog._allowed_type
 )
 catalog = catalog.search(lens_type=lens_type_option)
 
@@ -22,4 +24,4 @@ st.write(catalog.to_pandas())
 
 # Plot catalog
 plot_catalog(catalog)
-st.image("catalog.png")
+catalog_img.image("catalog.png")
