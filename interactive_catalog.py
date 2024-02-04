f9f4a8d8777a088104b19e6dbc7dde78ca320d21
Ka Lok (Rico) Lo <rico.kaloklo@gmail.com>
2024-02-05 00:25:43 +0100

    Add search by lens type

--- interactive_catalog.py
+++ interactive_catalog.py
@@ -2,7 +2,24 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 import matplotlib
+from matplotlib import pyplot as plt
 import lenscat
+from lenscat.utils import *
 
 st.title("Interactive Web App for lenscat")
-st.write(lenscat.catalog)
+# This catalog
+catalog = lenscat.catalog
+
+# Search by lens type
+lens_type_option = st.selectbox(
+    "Lens type",
+    (None, *lenscat.Catalog._allowed_type)
+)
+catalog = catalog.search(lens_type=lens_type_option)
+
+# Write catalog
+st.write(catalog.to_pandas())
+
+# Plot catalog
+plot_catalog(catalog)
+st.image("catalog.png")
