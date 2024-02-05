6826164d4ddb9a07a9de4079f23fd1d2e7d8ab16
Ka Lok (Rico) Lo <rico.kaloklo@gmail.com>
2024-02-05 01:04:34 +0100

    Add zlens filtering

--- interactive_catalog.py
+++ interactive_catalog.py
@@ -14,6 +14,12 @@ def convert_all_to_None(x):
     else:
         return x
 
+def convert_to_zlens_range(zlens_min):
+    if zlens_min is None:
+        return None # Do not filter
+    else:
+        return (zlens_min, np.inf)
+
 st.title("Interactive Web App for lenscat")
 # This catalog
 catalog = lenscat.catalog
@@ -46,9 +52,17 @@ grading_option = st.selectbox(
     "Grading",
     (_all, *lenscat.Catalog._allowed_grading),
 )
+# Search by lens redshift
+zlens_min = st.number_input(
+    "Minimum lens redshift",
+    min_value=0.0,
+    value=None,
+    step=0.1,
+)
 catalog = catalog.search(
     RA_range=RA_range,
     DEC_range=DEC_range,
+    zlens_range=convert_to_zlens_range(zlens_min),
     grading=convert_all_to_None(grading_option),
     lens_type=convert_all_to_None(lens_type_option),
 )
