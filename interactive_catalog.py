a17ae611a613309ab8d07b490da17be85ecce96b
Ka Lok (Rico) Lo <rico.kaloklo@gmail.com>
2024-02-05 14:56:46 +0100

    Update lenscat automatically

--- interactive_catalog.py
+++ interactive_catalog.py
@@ -3,6 +3,9 @@ import numpy as np
 import pandas as pd
 import matplotlib
 from matplotlib import pyplot as plt
+import sys
+import subprocess
+import importlib
 import lenscat
 from lenscat.utils import *
 from lenscat._version import __version__
@@ -20,6 +23,10 @@ def convert_to_zlens_range(zlens_min):
         return None # Do not filter
     else:
         return (zlens_min, np.inf)
+
+def update_lenscat():
+    subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'lenscat', '--upgrade'])
+    importlib.reload(lenscat)
 
 st.set_page_config(
     page_title="Interactive Web App for lenscat",
@@ -40,6 +47,8 @@ unsafe_allow_html=True
 )
 # Title
 st.latex(r"{\Huge \texttt{lenscat}}")
+# Update to the latest release of lenscat
+update_lenscat()
 # This catalog
 catalog = lenscat.catalog
 
