7b9c9d04602a3327eaaf3c58a3eb788b0ebd84d2
Ka Lok (Rico) Lo <rico.kaloklo@gmail.com>
2024-02-07 15:05:27 +0100

    Tracing bug

--- interactive_catalog.py
+++ interactive_catalog.py
@@ -20,15 +20,19 @@ def convert_deg_to_hms_str(deg):
     return _coord.to_string('hmsdms').split(' ')[0]
 
 def convert_hms_str_to_time(hms_str):
-    _h = int(hms_str.split('h')[0]) # Between 0 and 23
-    _m = int(hms_str.split('h')[1].split('m')[0]) # Between 0 and 59
-    _s = int(hms_str.split('h')[1].split('m')[1].split('s')[0].split('.')[0])
-    _microsec = hms_str.split('h')[1].split('m')[1].split('s')[0].split('.')[1]
-    if _microsec is None:
-        return time(hour=_h, minute=_m, second=_s)
-    else:
-        _microsec = int(_microsec.ljust(6, '0'))
-        return time(hour=_h, minute=_m, second=_s, microsecond=_microsec)
+    # Debug
+    try:
+        _h = int(hms_str.split('h')[0]) # Between 0 and 23
+        _m = int(hms_str.split('h')[1].split('m')[0]) # Between 0 and 59
+        _s = int(hms_str.split('h')[1].split('m')[1].split('s')[0].split('.')[0])
+        _microsec = hms_str.split('h')[1].split('m')[1].split('s')[0].split('.')[1]
+        if _microsec is None:
+            return time(hour=_h, minute=_m, second=_s)
+        else:
+            microsec = int(_microsec.ljust(6, '0'))
+            return time(hour=_h, minute=_m, second=_s, microsecond=_microsec)
+    except:
+        raise ValueError(hms_str)
 
 def convert_time_to_hms_str(t):
     hms_str = "{}h{}m{}.{}s".format(
